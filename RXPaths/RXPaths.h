d0674d4819a6fd93e31503817a34759ce3e08a8b
Rob Rix <rob.rix@me.com>
2012-04-08 10:50:42 -0400

    Initial Commit

---
+++ RXPaths/RXPaths.h
@@ -0,0 +1,13 @@
+//
+//  RXPaths.h
+//  RXPaths
+//
+//  Created by Rob Rix on 12-04-08.
+//  Copyright (c) 2012 Black Pixel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RXPaths : NSObject
+
+@end
---
+++ RXPaths/RXPaths.m
@@ -0,0 +1,13 @@
+//
+//  RXPaths.m
+//  RXPaths
+//
+//  Created by Rob Rix on 12-04-08.
+//  Copyright (c) 2012 Black Pixel. All rights reserved.
+//
+
+#import "RXPaths.h"
+
+@implementation RXPaths
+
+@end
