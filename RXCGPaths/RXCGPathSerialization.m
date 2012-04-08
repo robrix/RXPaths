1fe275b14da1570ba159b736a6783b2bfc2e00aa
Rob Rix <rob.rix@me.com>
2012-04-08 13:33:18 -0400

    Implements and tests serialization of paths.

--- RXCGPaths/RXCGPathSerialization.m
+++ RXCGPaths/RXCGPathSerialization.m
@@ -4,6 +4,70 @@
 
 #import "RXCGPathSerialization.h"
 
+typedef void (*RXCGPathSerializeElementFunction)(NSMutableData *data, const CGPathElement *element);
+
 @implementation RXCGPathSerialization
+
+static void RXCGPathSerializePoint(NSMutableData *data, CGPoint point) {
+	const char format[100];
+	snprintf((char *)format, sizeof(format), "%.8g %.8g", point.x, point.y);
+	
+	[data appendBytes:format length:strlen(format)];
+}
+
+
+static void RXCGPathSerializeMoveElement(NSMutableData *data, const CGPathElement *element) {
+	[data appendBytes:"M" length:1];
+	RXCGPathSerializePoint(data, element->points[0]);
+}
+
+static void RXCGPathSerializeLineElement(NSMutableData *data, const CGPathElement *element) {
+	[data appendBytes:"L" length:1];
+	RXCGPathSerializePoint(data, element->points[0]);
+}
+
+static void RXCGPathSerializeQuadCurveElement(NSMutableData *data, const CGPathElement *element) {
+	[data appendBytes:"Q" length:1];
+	RXCGPathSerializePoint(data, element->points[0]);
+	[data appendBytes:" " length:1];
+	RXCGPathSerializePoint(data, element->points[1]);
+}
+
+static void RXCGPathSerializeCurveElement(NSMutableData *data, const CGPathElement *element) {
+	[data appendBytes:"C" length:1];
+	RXCGPathSerializePoint(data, element->points[0]);
+	[data appendBytes:" " length:1];
+	RXCGPathSerializePoint(data, element->points[1]);
+	[data appendBytes:" " length:1];
+	RXCGPathSerializePoint(data, element->points[2]);
+}
+
+static void RXCGPathSerializeCloseElement(NSMutableData *data, const CGPathElement *element) {
+	[data appendBytes:"Z" length:1];
+}
+
+static void RXCGPathSerializeElement(void *info, const CGPathElement *element) {
+	NSMutableData *data = *(NSMutableData * __strong *)info;
+	
+	static RXCGPathSerializeElementFunction elementFunctions[] = {
+		RXCGPathSerializeMoveElement,
+		RXCGPathSerializeLineElement,
+		RXCGPathSerializeQuadCurveElement,
+		RXCGPathSerializeCurveElement,
+		RXCGPathSerializeCloseElement
+	};
+	
+	(elementFunctions[element->type])(data, element);
+}
+
++(NSData *)dataWithPath:(CGPathRef)path error:(NSError * __autoreleasing *)error {
+	NSParameterAssert(path != NULL);
+	
+	NSMutableData *data = [NSMutableData new];
+	
+	CGPathApply(path, &data, RXCGPathSerializeElement);
+	
+	return data;
+}
 
 @end
--- Tests/RXCGPathSerializationTests.m
+++ Tests/RXCGPathSerializationTests.m
@@ -3,14 +3,60 @@
 //  Copyright (c) 2012 Black Pixel. All rights reserved.
 
 #import "RXAssertions.h"
+#import "RXCGPathSerialization.h"
 
 @interface RXCGPathSerializationTests : SenTestCase
 @end
 
 @implementation RXCGPathSerializationTests
 
--(void)test<#Case#> {
-	<##>
+-(NSString *)serializedPath:(CGPathRef)path {
+	NSError *error = nil;
+	NSData *data = [RXCGPathSerialization dataWithPath:path error:&error];
+	RXAssertNotNil(data, @"%@", error);
+	return data? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : nil;
+}
+
+-(void)testSerializesMoves {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, 10, 10);
+	RXAssertEquals([self serializedPath:path], @"M10 10");
+}
+
+-(void)testSerializesFractionalMoves {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, M_PI, M_PI);
+	RXAssertEquals([self serializedPath:path], @"M3.1415927 3.1415927");
+}
+
+
+-(void)testSerializesLines {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, 0, 0);
+	CGPathAddLineToPoint(path, NULL, 10, 10);
+	RXAssertEquals([self serializedPath:path], @"M0 0L10 10");
+}
+
+-(void)testSerializesQuadraticCurves {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, 0, 0);
+	CGPathAddQuadCurveToPoint(path, NULL, 10, 10, 20, 20);
+	RXAssertEquals([self serializedPath:path], @"M0 0Q10 10 20 20");
+}
+
+-(void)testSerializesCubicCurves {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, 0, 0);
+	CGPathAddCurveToPoint(path, NULL, 10, 10, 20, 20, 30, 30);
+	RXAssertEquals([self serializedPath:path], @"M0 0C10 10 20 20 30 30");
+}
+
+-(void)testSerializesCloses {
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, NULL, 0, 0);
+	CGPathAddLineToPoint(path, NULL, 10, 10);
+	CGPathCloseSubpath(path);
+	RXAssertEquals([self serializedPath:path], @"M0 0L10 10Z");
 }
 
 @end
